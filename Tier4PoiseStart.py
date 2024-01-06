01d9fde995fe9fbab7dd5005f9513a9b90ca390f
DT <dt92203@gmail.com>
2024-01-05 18:08:23 -0800

    slowly making progress implemented try and excepts for each step in the process of finding the poise components

--- Tier4PoiseStart.py
+++ Tier4PoiseStart.py
@@ -7,9 +7,12 @@ import sys
 import time
 import psutil
 
+# MD3N stands for mirror dungeon 3 normal
 # Region to select gifts (1148, 330) (1761, 814)
 # Region to select mirror dungeon normal (130, 261) (555, 891)
 # Region to select
+
+# Future, use pictures of gifts instead of the name
 def getMouseCoord():
     try:
         while True:
@@ -22,24 +25,62 @@ def getMouseCoord():
     except KeyboardInterrupt:
         print('\n')
 
+
+
+def refreshGifts():
+    try:
+        Refresh = pyautogui.locateOnScreen("Refresh.png", grayscale=True, confidence=.9)
+        pyautogui.moveTo(Refresh)
+        time.sleep(1)
+
+    except:
+        print("Could not refresh the gifts")
+
+
 def main():
-    gameIsOpen= "LimbusCompany.exe" in (i.name() for i in psutil.process_iter())
+    foundClover, foundHorseshoe = False
+    gameIsOpen = "LimbusCompany.exe" in (i.name() for i in psutil.process_iter())
     if gameIsOpen:
         print("Running Script")
 
-
+        # Entering mirror dungeon
         try:
             MD3N = pyautogui.locateOnScreen("MD3N.png", grayscale=True, confidence=.9, region=(130, 261, 425, 761))
             pyautogui.moveTo(MD3N)
             pyautogui.leftClick()
+            pyautogui.sleep(.5)
+            pyautogui.press("enter")
+            time.sleep(1)
         except:
             print("Failed to click on Mirror Dungeon")
 
+        # Picking poise
         try:
-            Horseshoe = pyautogui.locateOnScreen("Horseshoe.png", grayscale=True, confidence=.5, region=(1148, 330, 613, 484))
+            Poise = pyautogui.locateOnScreen("Poise.png", grayscale=True, confidence=.9)
+            pyautogui.moveTo(Poise)
+            pyautogui.leftClick()
+            time.sleep(.5)
+        except:
+            print("Failed to find poise")
+
+        # while(!foundClover && !foundHorseshoe):
+        try:
+            Clover = pyautogui.locateOnScreen("Clover.png", grayscale=True, confidence=.9)
+            pyautogui.moveTo(Clover)
+            pyautogui.leftClick()
+            foundClover = True
+        except:
+            print("Failed to grab clover")
+            refreshGifts()
+
+        try:
+            Horseshoe = pyautogui.locateOnScreen("Horseshoe.png", grayscale=True, confidence=.9, region=(1148, 330, 613, 484))
             pyautogui.moveTo(Horseshoe)
+            foundHorseshoe = True
         except:
             print("Failed to grab horseshoe")
+            refreshGifts()
+
 
 
 
@@ -48,3 +89,4 @@ def main():
         sys.exit()
 
 main()
+
