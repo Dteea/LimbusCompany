a3cf5803c94c724f8ce2c1ec0b0f1f835b3058d2
DT <dt92203@gmail.com>
2024-01-05 17:19:57 -0800

    Everything seems to work now, you can now click on mirror dungeon

--- Tier4PoiseStart.py
+++ Tier4PoiseStart.py
@@ -3,12 +3,13 @@
 # are found.
 
 import pyautogui
-import cv2
 import sys
 import time
+import psutil
 
 # Region to select gifts (1148, 330) (1761, 814)
-
+# Region to select mirror dungeon normal (130, 261) (555, 891)
+# Region to select
 def getMouseCoord():
     try:
         while True:
@@ -22,7 +23,28 @@ def getMouseCoord():
         print('\n')
 
 def main():
-    Horseshoe = pyautogui.locateOnScreen("Horseshoe.png", confidence=.5, region=(1148, 330, 613, 484))
-    print(Horseshoe)
-    pyautogui.moveTo(Horseshoe)
+    gameIsOpen= "LimbusCompany.exe" in (i.name() for i in psutil.process_iter())
+    if gameIsOpen:
+        print("Running Script")
+
+
+        try:
+            MD3N = pyautogui.locateOnScreen("MD3N.png", grayscale=True, confidence=.9, region=(130, 261, 425, 761))
+            pyautogui.moveTo(MD3N)
+            pyautogui.leftClick()
+        except:
+            print("Failed to click on Mirror Dungeon")
+
+        try:
+            Horseshoe = pyautogui.locateOnScreen("Horseshoe.png", grayscale=True, confidence=.5, region=(1148, 330, 613, 484))
+            pyautogui.moveTo(Horseshoe)
+        except:
+            print("Failed to grab horseshoe")
+
+
+
+    else:
+        print("Could not find Limbus Company, Exiting")
+        sys.exit()
+
 main()
