cf61836356c6b94208a9491868a99ec970874231
DT <dt92203@gmail.com>
2024-01-07 15:21:39 -0800

    separated steps into functions, program can now handle cases of the components, yippe

--- Tier4PoiseStart.py
+++ Tier4PoiseStart.py
@@ -6,11 +6,11 @@ import pyautogui
 import sys
 import time
 import psutil
+pyautogui.PAUSE = .6
 
 # MD3N stands for mirror dungeon 3 normal
 # Region to select gifts (1148, 330) (1761, 814)
 # Region to select mirror dungeon normal (130, 261) (555, 891)
-# Region to select
 
 # Future, use pictures of gifts instead of the name
 def getMouseCoord():
@@ -29,41 +29,29 @@ def getMouseCoord():
 
 def refreshGifts():
     try:
-        Refresh = pyautogui.locateOnScreen("Refresh.png", grayscale=True, confidence=.9)
-        pyautogui.moveTo(Refresh)
-        time.sleep(1)
+        Refresh = pyautogui.locateOnScreen("Refresh.png",
+                                           grayscale=True,
+                                           confidence=.9)
 
+        pyautogui.moveTo(Refresh)
+        pyautogui.leftClick()
     except:
         print("Could not refresh the gifts")
 
-
-def main():
-    foundClover, foundHorseshoe = False
-    gameIsOpen = "LimbusCompany.exe" in (i.name() for i in psutil.process_iter())
-    if gameIsOpen:
-        print("Running Script")
-
-        # Entering mirror dungeon
-        try:
-            MD3N = pyautogui.locateOnScreen("MD3N.png", grayscale=True, confidence=.9, region=(130, 261, 425, 761))
-            pyautogui.moveTo(MD3N)
-            pyautogui.leftClick()
-            pyautogui.sleep(.5)
-            pyautogui.press("enter")
-            time.sleep(1)
-        except:
-            print("Failed to click on Mirror Dungeon")
-
+#Idk some recursion, maybe do something after returning to the first call of this function
+def findPoiseGift(isRefreshed):
+        foundClover = False
+        foundHorseshoe = False
         # Picking poise
         try:
-            Poise = pyautogui.locateOnScreen("Poise.png", grayscale=True, confidence=.9)
+            Poise = pyautogui.locateOnScreen("Poise.png",
+                                             grayscale=True,
+                                             confidence=.9)
             pyautogui.moveTo(Poise)
             pyautogui.leftClick()
-            time.sleep(.5)
         except:
             print("Failed to find poise")
 
-        # while(!foundClover && !foundHorseshoe):
         try:
             Clover = pyautogui.locateOnScreen("Clover.png", grayscale=True, confidence=.9)
             pyautogui.moveTo(Clover)
@@ -71,22 +59,55 @@ def main():
             foundClover = True
         except:
             print("Failed to grab clover")
-            refreshGifts()
+            if not isRefreshed:
+                refreshGifts()
+                isRefreshed = True
+                findPoiseGift(isRefreshed)
 
+        if foundClover == True:
+            try:
+                Horseshoe = pyautogui.locateOnScreen("Horseshoe.png",
+                                                     grayscale=True,
+                                                     confidence=.9,
+                                                     region=(1148, 330, 613, 484))
+                pyautogui.moveTo(Horseshoe)
+                foundHorseshoe = True
+            except:
+                print("Failed to grab horseshoe")
+                if not isRefreshed:
+                    refreshGifts()
+                    isRefreshed = True
+                    findPoiseGift(isRefreshed)
+
+        if (foundClover and foundHorseshoe):
+            print("Success")
+
+
+def main():
+    gameIsOpen = "LimbusCompany.exe" in (i.name() for i in psutil.process_iter())
+    isRefreshed = False
+
+    if gameIsOpen:
+        print("Running Script")
+
+        # Entering mirror dungeon
         try:
-            Horseshoe = pyautogui.locateOnScreen("Horseshoe.png", grayscale=True, confidence=.9, region=(1148, 330, 613, 484))
-            pyautogui.moveTo(Horseshoe)
-            foundHorseshoe = True
+            MD3N = pyautogui.locateOnScreen("MD3N.png",
+                                            grayscale=True,
+                                            confidence=.9,
+                                            region=(130, 261, 425, 761))
+            pyautogui.moveTo(MD3N)
+            pyautogui.leftClick()
+            pyautogui.press("enter")
         except:
-            print("Failed to grab horseshoe")
-            refreshGifts()
+            print("Failed to click on Mirror Dungeon")
 
-
-
+        findPoiseGift(isRefreshed)
 
     else:
         print("Could not find Limbus Company, Exiting")
         sys.exit()
+
 
 main()
 
