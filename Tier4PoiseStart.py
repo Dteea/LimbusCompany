1574090f591e573bdc065fe8a47208e7f959d82e
DT <dt92203@gmail.com>
2024-01-07 18:09:59 -0800

    More separate functions, everything seems to be working

--- Tier4PoiseStart.py
+++ Tier4PoiseStart.py
@@ -7,7 +7,6 @@ import sys
 import time
 import psutil
 pyautogui.PAUSE = .6
-
 # MD3N stands for mirror dungeon 3 normal
 # Region to select gifts (1148, 330) (1761, 814)
 # Region to select mirror dungeon normal (130, 261) (555, 891)
@@ -38,10 +37,43 @@ def refreshGifts():
     except:
         print("Could not refresh the gifts")
 
+def exitAndClear(MD3N):
+    try:
+        #goBack = pyautogui.locateOnScreen("exitArrow.png", grayscale=True, confidence=.9)
+        pyautogui.press("escape")
+        pyautogui.moveTo(MD3N)
+        pyautogui.leftClick()
+        Halt = pyautogui.locateOnScreen("Halt.png",
+                                 grayscale=True,
+                                 confidence=.9)
+        pyautogui.moveTo(Halt)
+        pyautogui.leftClick()
+        pyautogui.press("enter")
+        giveUp = pyautogui.locateOnScreen("GiveUp.png",
+                                 grayscale=True,
+                                 confidence=.9)
+        pyautogui.moveTo(giveUp)
+        pyautogui.leftClick()
+        pyautogui.press("enter")
+    except:
+        print("Failed to exit E.G.O gift selection")
+
+def enterDungeon(MD3N):
+
+
+    try:
+        pyautogui.moveTo(MD3N)
+        pyautogui.leftClick()
+        pyautogui.press("enter")
+    except:
+        print("Failed to click on Mirror Dungeon")
+
+
 #Idk some recursion, maybe do something after returning to the first call of this function
 def findPoiseGift(isRefreshed):
         foundClover = False
         foundHorseshoe = False
+        foundGifts = False
         # Picking poise
         try:
             Poise = pyautogui.locateOnScreen("Poise.png",
@@ -81,33 +113,38 @@ def findPoiseGift(isRefreshed):
 
         if (foundClover and foundHorseshoe):
             print("Success")
+            foundGifts = True
+
+        return foundGifts
 
 
 def main():
-    gameIsOpen = "LimbusCompany.exe" in (i.name() for i in psutil.process_iter())
-    isRefreshed = False
 
+    gameIsOpen = "LimbusCompany.exe" in (i.name() for i in psutil.process_iter())
+    foundGift = False
+
+    MD3N = pyautogui.locateOnScreen("MD3N.png",
+                                    grayscale=True,
+                                    confidence=.9,
+                                    region=(130, 261, 425, 761))
     if gameIsOpen:
         print("Running Script")
-
+        # TODO add key to exit the program whenever, keep track of how many times it takes to reroll and
+        # TODO maybe do some averages
         # Entering mirror dungeon
-        try:
-            MD3N = pyautogui.locateOnScreen("MD3N.png",
-                                            grayscale=True,
-                                            confidence=.9,
-                                            region=(130, 261, 425, 761))
-            pyautogui.moveTo(MD3N)
-            pyautogui.leftClick()
-            pyautogui.press("enter")
-        except:
-            print("Failed to click on Mirror Dungeon")
+        enterDungeon(MD3N)
 
-        findPoiseGift(isRefreshed)
+        while not foundGift:
+            enterDungeon(MD3N)
+            isRefreshed = False
+            foundGift = findPoiseGift(isRefreshed)
+            if not foundGift:
+                exitAndClear(MD3N)
+
 
     else:
         print("Could not find Limbus Company, Exiting")
         sys.exit()
-
 
 main()
 
